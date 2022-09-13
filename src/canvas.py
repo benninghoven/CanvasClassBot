c7554ef7d430b7998b77934454b2e464e6beab1d
Joel <100006388+joelrico@users.noreply.github.com>
2022-09-12 21:15:36 -0700

    Added various functions

--- src/canvas.py
+++ src/canvas.py
@@ -6,33 +6,46 @@ from canvasapi import Canvas
 # Canvas API URL
 API_URL = "https://canvas.instructure.com/"
 # Canvas API key
-API_KEY = "token"
+API_KEY = "349~NTHNMprd3AAqhwgIY8veN9PrZvH8VZ8TUaouLIpyYBDCHwdQ0VX1ebOaIC8RRYUt"
 
 # Initialize a new Canvas object
 canvas = Canvas(API_URL, API_KEY)
 
 # Initialize user
 user = canvas.get_current_user()
-set_course = None
+
+
+def list_assignments(course):
+    """Lists public assignments for course"""
+
+    i = 1
+    for assignment in course.get_assignments():
+        print("  " + str(i) + ") " + str(assignment))
+        i += 1
+
+    if i == 1:
+        print("  None")
+
+
+def list_courses():
+    """Lists courses for current user"""
+
+    i = 1
+    for course in user.get_courses():
+        print(str(i) + ") " + course.name)
+        i += 1
+
+
+def find_course(query):
+    """Finds first instance of course matching course_name"""
+
+    for course in user.get_courses():
+        if query.lower() in str(course.name).lower():
+            return course
+
 
 user_input = input("Input course name: ")
+set_course = find_course(user_input)
 
-# Finds first instance of course matching course_name
-for course in user.get_courses():
-    if user_input.lower() in str(course.name).lower():
-        set_course = course
-        break
-
-# Print course name
-print("---------------------")
-print(set_course.name)
-
-# List of all assignments
-i = 1
-print("  Assignments")
-for assignment in set_course.get_assignments():
-    print("  " + str(i) + ") " + str(assignment))
-    i += 1
-
-if i == 1:
-    print("  None")
+list_assignments(set_course)
+list_courses()
