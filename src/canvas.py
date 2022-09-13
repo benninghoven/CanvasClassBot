ea75868e45cb5a89c95f0002263ad9b396ffdbaf
Joel <100006388+jjoeldaniel@users.noreply.github.com>
2022-09-13 08:50:59 -0700

    Functions now return lists instead of directly printing

--- src/canvas.py
+++ src/canvas.py
@@ -16,28 +16,37 @@ user = canvas.get_current_user()
 
 
 def list_assignments(course):
-    """Lists public assignments for course"""
+    """Returns list of public assignments for course
+        Returns null if no assignments found"""
 
-    i = 1
+    assignments = list()
+
     for assignment in course.get_assignments():
-        print("  " + str(i) + ") " + str(assignment))
-        i += 1
+        assignments.append(str(assignment))
 
-    if i == 1:
-        print("  None")
+    if len(assignments) == 0:
+        return None
+    else:
+        return assignments
 
 
 def list_courses():
-    """Lists courses for current user"""
+    """Returns list of courses for current user
+        Returns null if no courses found"""
 
-    i = 1
+    courses = list()
+
     for course in user.get_courses():
-        print(str(i) + ") " + course.name)
-        i += 1
+        courses.append(course.name)
+
+    if len(courses) == 0:
+        return None
+    else:
+        return courses
 
 
 def find_course(query):
-    """Finds first instance of course matching course_name"""
+    """Returns first instance of course matching course_name"""
 
     for course in user.get_courses():
         if query.lower() in str(course.name).lower():
@@ -47,5 +56,5 @@ def find_course(query):
 user_input = input("Input course name: ")
 set_course = find_course(user_input)
 
-list_assignments(set_course)
-list_courses()
+assignments = list_assignments(set_course)
+courses = list_courses()
